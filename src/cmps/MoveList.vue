c8683f5c672c058ea500edbc4cc78bd0228e7bb8
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-12-07 22:05:03 +0200

    F/adding move-list and move priview components

---
+++ src/cmps/MoveList.vue
@@ -0,0 +1,49 @@
+<template>
+    <section class="move-list-container" v-if="user">
+    <h1>Your Moves</h1>
+    <ul class="move-list">
+            <li v-for="move in loadMoves" :key="move.toId">
+                    <MovePreview :move="move" />
+            </li>
+    </ul>
+</section>
+</template>
+
+<script>
+import MovePreview from './MovePreview.vue';
+    export default {
+        props:{
+            contact: {
+                type: Object,
+                required: true,
+            },
+            user:{
+                type: Object,
+                required: true,
+            }
+        },
+        data(){
+            return{
+                // loadMoves: user.transactions.filter(move=> move.toId===this.contact._id)
+            }
+        },
+        methods:{
+           
+        },
+        computed:{
+            loadMoves(){
+                return this.user.transactions.filter(move=> move.toId===this.contact._id)
+            }
+        },
+        // created(){
+        //     console.log(this.loadMoves());
+        // },
+        components:{
+            MovePreview
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/cmps/MovePreview.vue
@@ -0,0 +1,23 @@
+<template>
+    <article class="move-preview">
+    <p><span>To </span>{{move.to}}</p>
+    <p><span>At </span>{{move.at | date}}</p>
+    <p><span>Amount </span>{{move.amount}}</p>
+</article>
+
+</template>
+
+<script>
+    export default {
+        props:{
+            move: {
+                type: Object,
+                required: true,
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+
+</style>
