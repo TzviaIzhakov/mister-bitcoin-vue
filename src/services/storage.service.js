bb28cad1d447de855129e3dc40be70239d8ded96
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-12-07 22:59:28 +0200

    F/adding signup functuality in service

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -6,6 +6,7 @@ export const storageService = {
 function saveToStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value) || null)
 }
+
 function loadFromStorage(key) {
     let data = localStorage.getItem(key)
     return data ? JSON.parse(data) : undefined
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,16 @@
+import { dbService } from "./db.service"
+import { storageService } from '../services/storage.service.js'
+
 export const userService = {
     getUser,
+    getEmptyUser,
+    saveUser,
+    addMove,
+    loadMoves
 }
+
+const STORAGE_KEY = 'user'
+const LOGGEDINUSER = 'loggedUser'
 const gUser = {
     name: "Tzvia Izhakov",
     balance: 200,
@@ -8,5 +18,47 @@ const gUser = {
 }
 
 function getUser(){
-    return gUser
+  return storageService.load(LOGGEDINUSER)
+}
+
+function getEmptyUser(){
+    return {name:'', balance:100, transactions: []}
+}
+
+async function saveUser(user) {
+    if (await isUsernameExists(user.name)) {
+        const existedUser  =await getUserByName(user.name);
+        storageService.save(LOGGEDINUSER,existedUser)
+        return existedUser
+    } 
+    else{
+        const savedUser = await dbService.post(STORAGE_KEY, user)
+        storageService.save(LOGGEDINUSER, savedUser)
+        return savedUser
+    }
+}
+
+async function isUsernameExists(name) {
+    let users = await dbService.query(STORAGE_KEY)
+    let user 
+    if(users && users.length){
+        user = users.find(user=> user.name===name)
+    }
+    return user
+}
+
+async function getUserByName(name) {
+    let users = await dbService.query(STORAGE_KEY)
+    return users.find(user=>user.name===name)
+} 
+
+async function addMove(user) {
+    console.log("service", user);
+    storageService.save(LOGGEDINUSER,user)
+    await dbService.put(STORAGE_KEY,user)
+}
+
+function loadMoves(contact) {
+    const user = getUser()
+    return user.transactions.filter(move=> move.toId===contact._id)
 }
