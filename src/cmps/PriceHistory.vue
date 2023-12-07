4bbe3ec70970e532128854d76fbf82367a950e34
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-12-07 22:06:20 +0200

    F/adding differnent charts

---
+++ src/cmps/PriceHistory.vue
@@ -0,0 +1,75 @@
+<template>
+    <Bar
+    :data="chartData"
+    :options="chartOptions"
+    :id="chartId"
+    :style="myStyles"
+   />
+  </template>
+  
+  <script lang="ts">
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+  } from "chart.js"
+    import { Bar } from "vue-chartjs"
+
+  ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+)
+  
+  export default {
+    components: {
+      Bar
+    },
+    props: {
+    chartId: {
+      type: String,
+      default: "bar-chart",
+    },
+    datasetIdKey: {
+      type: String,
+      default: "label",
+    },
+    labels: {
+      type: Array,
+      default: () => [],
+    },
+    data: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  data() {
+    return {
+      chartData: {
+        labels: this.labels,
+        datasets: [{ data: this.data, backgroundColor: '#f87979',label: 'price history' }],
+      },
+      chartOptions: {
+        responsive: true,
+      },
+    };
+  },
+  computed: {
+    myStyles () {
+      return {
+        height: '40px',
+        width: '50px'
+      }
+    }
+  }
+  }
+  </script>
+  
---
+++ src/cmps/TransactionsHistory.vue
@@ -0,0 +1,61 @@
+<template>
+      <Line
+        :data="chartData"
+        :options="chartOptions"
+        :width="50"
+        :height="70"
+      />
+  </template>
+  
+  <script>
+  import { Line } from "vue-chartjs";
+  import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  } from "chart.js"
+
+    ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+    )
+  export default {
+    props:{
+        labels: {
+            type: Array,
+            default: () => [],
+            },
+        data: {
+            type: Array,
+            default: () => [],
+        },
+    },
+    data() {
+    return {
+      chartData: {
+        labels: this.labels,
+        datasets: [{ data: this.data , backgroundColor: 'green',label: 'transaction history'}],
+      },
+      chartOptions: {
+        responsive: true,
+      },
+    };
+  },
+    components: { Line }
+  };
+  </script>
+  
+  <style scoped>
+  /* Add custom styles as needed */
+  </style>
+  
