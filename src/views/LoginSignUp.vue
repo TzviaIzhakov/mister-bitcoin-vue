338ff26a9d12ba2d23a63ea8a5efdc1dd8fde8e0
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-12-07 22:07:36 +0200

    F/adding signuplogin component without password as requested

---
+++ src/views/LoginSignUp.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="login-signup-container">
+        <form class="form-login-signup" @submit.prevent="onSave">
+            <input type="text" class="username-field" v-model="user.name">
+            <button>Submit</button>
+        </form>
+
+    </section>
+</template>
+
+<script>
+    import {userService} from '../services/userService'
+    export default {
+        data(){
+            return{
+                user : userService.getEmptyUser()
+            }
+        },
+        computed:{
+
+        },
+        methods:{
+            async onSave(){
+                await userService.saveUser(this.user)
+                this.$router.push('/')
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss">
+
+</style>
