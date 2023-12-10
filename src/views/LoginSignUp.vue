ef90b3e369ee4bc8ebdf582e9d54bc12b4d86d07
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-12-10 11:53:35 +0200

    F/styling loginsignup

--- src/views/LoginSignUp.vue
+++ src/views/LoginSignUp.vue
@@ -1,8 +1,9 @@
 <template>
     <section class="login-signup-container">
+        <h1>Sign Up</h1>
         <form class="form-login-signup" @submit.prevent="onSave">
-            <input type="text" class="username-field" v-model="user.name">
-            <button>Submit</button>
+            <input type="text" class="username-field" v-model="user.name" placeholder="Enter your fullname">
+            <button class="btn-regular">Submit</button>
         </form>
 
     </section>
@@ -30,5 +31,26 @@
 </script>
 
 <style lang="scss">
-
+.login-signup-container{
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    h1{
+            font-weight: 700;
+            color: orange;
+            margin-bottom: 45px;
+    }
+    .form-login-signup{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 12px;
+        box-shadow: 0 4px 12px #23232329;
+        padding-block: 24px;
+        padding-inline: 15px;
+        button{
+            align-self: center;
+        }
+    }
+}
 </style>
